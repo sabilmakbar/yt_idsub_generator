e2c07cf9f59a095572f23e31dfdc5cb791aa4341
sabilmakbar <maulana.1998@yahoo.co.id>
2022-02-06 01:20:11 +0700

    Updating codebase files and restucturing folders

--- ST Scripts/main.py
+++
@@ -1,3 +0,0 @@
-from VideoLinkScraper import yt_scrapper_link
-
-yt_scrapper_link("https://www.youtube.com/channel/UCREgA-BmOocJ9Is_bZV6aJQ/videos")
---
+++ ST Scripts/yt_scraper.py
@@ -0,0 +1,200 @@
+#to install package from virtual env, cd to {virtualenv_path}/bin 
+#then do ./python pip install 
+
+import time, datetime
+
+import urllib.request, urllib.error, urllib.parse
+
+from selenium import webdriver
+import subprocess
+
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import InvalidArgumentException
+from selenium.webdriver.chrome.options import Options
+
+
+from requests_html import AsyncHTMLSession 
+from bs4 import BeautifulSoup as bs # importing BeautifulSoup
+
+import asyncio
+
+import yt_dlp
+import os
+
+import webvtt
+import pandas as pd
+import numpy as np
+
+# A Python Selenium Scraper for Retrieve the List of Links from A Channel
+def channel_video_link_scraper(channel_url : str):
+    """Retrieving a list of all public videos in the given channel URL
+    input: channel_url (str) -- an url link of input channel to be scraped
+    output: video_list (list of str) -- a list of all public videos uploaded in that channel
+    Reference: https://github.com/banhao/scrape-youtube-channel-videos-url/blob/master/scrape-youtube-channel-videos-url.py
+    """
+
+    proc = subprocess.Popen('apt install chromium-chromedriver', shell=True, stdin=None, stdout=open("/dev/null", "w"), stderr=None, executable="/bin/bash")
+    proc.wait()
+
+    try:
+        channelid = channel_url.split('/')[4]
+    except:
+        raise ValueError("The variable url {} is not a valid YT Channel Link".format(channel_url))
+    
+    chrome_options = Options()
+    chrome_options.add_argument("--user-data-dir=chrome-data")
+    chrome_options.add_argument("--headless")
+
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+
+    driver.get(channel_url)
+    time.sleep(5)
+    dt=datetime.datetime.now().strftime("%Y%m%d%H%M")
+    height = driver.execute_script("return document.documentElement.scrollHeight")
+    lastheight = 0
+
+    while True:
+        if lastheight == height:
+            break
+        lastheight = height
+        driver.execute_script("window.scrollTo(0, " + str(height) + ");")
+        time.sleep(2)
+        height = driver.execute_script("return document.documentElement.scrollHeight")
+
+    user_data = driver.find_elements_by_xpath('//*[@id="video-title"]')
+    
+    print("The total number of videos catched is: {}".format(len(user_data)))
+
+    video_list = [data.get_attribute('href') for data in user_data]
+    
+    return video_list
+
+# A Python Metadata Collection for Retrieve the Video Duration
+async def async_yt_metadata_scraper(*args):
+    """Async Method for Retrieving metadata of given public videos.
+    Has to be called within function "yt_metadata_scraper" to make the output works
+    as intended.
+    It follows the input from "yt_metadata_scraper"
+    Reference: https://www.thepythoncode.com/article/get-youtube-data-python
+    """
+
+    video_url, timeout = args
+
+    # init an HTML Session
+    session = AsyncHTMLSession()
+    # get the html content
+    try:
+        response = await session.get(video_url)
+    except TimeoutError as Te:
+        print(Te[:-1]+", need to increase default timeout or retry again")
+        return None
+    except Exception as e:
+        print(e)
+
+    # execute Java-script
+    await response.html.arender(sleep=1, timeout = timeout)
+    # create bs object to parse HTML
+    soup = bs(response.html.raw_html, "html.parser")
+
+    upload_date = soup.find("meta", itemprop="uploadDate")['content']
+    duration = soup.find("span", {"class": "ytp-time-duration"}).text
+    
+    duration_splitted = duration.split(":")
+    if len(duration_splitted) > 3:
+        print("Warning, the duration is >24 h. Returning duration_s variable as None!")
+        duration_s = None
+    else:
+        duration_s = int(duration_splitted[-1])
+        try:
+            duration_s += int(duration_splitted[-2])*60
+        except:
+            pass
+        else:
+            try:
+                duration_s += int(duration_splitted[-3])*3600
+            except:
+                pass
+    
+    # get the uploaded date
+    output_dict = {"duration": duration,
+                    "duration_s": duration_s,
+                    "upload_date": upload_date}
+
+    return output_dict
+
+def yt_metadata_scraper(video_url: str, timeout : int = 60):
+    """Retrieving a dict of metadata from YT URL input using asyncronous method
+    input: 
+        channel_url (str) -- an url link of input video to be retrieved of its metadata
+        timeout (int, optional) -- a timeout variable for waiting response
+    output: output_dict (dict) -- a dict of metadata, which can be found on function "async_yt_metadata_scraper"
+    """
+
+    if asyncio.get_event_loop().is_running(): # Only patch if needed (i.e. running in Notebook, Spyder, etc)
+        import nest_asyncio
+        nest_asyncio.apply()
+
+    return asyncio.run(async_yt_metadata_scraper(video_url, timeout))
+
+def yt_subtitle_downloader(video_urls: list, folder_path_to_save: str = os.getcwd(), ydl_opts : dict=None):
+    """Retrieving subtitle info and timestamp from YT URL input.
+    It will save the downloaded files into "path_to_save" variables.
+    It is recommended to fill in folder_path_to_save rather than leave it as default.
+    input: 
+        channel_url (list of str) -- a list of url links of input video to be scraped
+        path_to_save (str, optional) -- a path for storing result (defaults to current working directory)
+    """
+
+    #check if "youtube.com" contains in the list input
+    for url in video_urls:
+        if "youtube.com" not in url:
+            raise ValueError("This string 'youtube.com' is not in {}, it's not a default YT link!".format(url))
+    
+    #reference on outtmpl: https://stackoverflow.com/questions/32482230/how-to-set-up-default-download-location-in-youtube-dl/34958672
+    if ydl_opts is None:
+        ydl_opts = {
+            'format': 'bv*[height<=480][ext=mp4]+ba[ext=m4a]/b[height<=480][ext=mp4] / wv*+ba/w', #Ensures best settings
+            'writesubtitles': True, #Adds a subtitles file if it exists
+            'writeautomaticsub': True, #Adds auto-generated subtitles file
+            'subtitle': '--sub-lang en', #writes subtitles file in english
+            'skip_download': True, #skips downloading the video file, if we want to download the vid just change into false
+            'outtmpl': folder_path_to_save + '/%(title)s.%(ext)s'
+        }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(video_urls)
+
+def yt_subtitle_file_vtt_to_csv_converter(saved_folder_path: str = os.getcwd()):
+    """Changing and parsing all file from vtt format (yt-dl download file) to csv.
+    It will check all .vtt files from "saved_folder_path" variables,
+    then parse and reformat it into dataframe csv format and delete the .vtt file.
+    input: 
+        channel_url (list of str) -- a list of url links of input video to be scraped
+        path_to_save (str, optional) -- a path for storing result (defaults to current working directory)
+    """
+
+    filenames = [file for file in os.listdir(saved_folder_path) if os.fsdecode(file).endswith(".vtt")]
+
+    #Extract the text and times from the vtt file
+    for file in filenames:
+        captions = webvtt.read(os.path.join(saved_folder_path,file))
+
+        #Create dataframe of subtitle filled with start and stop time, and also the text
+        text_time = pd.DataFrame()
+        text_time['text'] = [caption.text for caption in captions]
+        text_time['start'] = [caption.start for caption in captions]
+        text_time['stop'] = [caption.end for caption in captions]
+
+        #Replace duplicate values that was indicated by /n
+        text_time['text'] = text_time['text'].str.split('\n').str.get(-1)
+        text_time = text_time.replace(r'^\s*$', np.nan, regex=True).dropna()
+
+        #convert to csv
+        text_time.to_csv('{}/{}.csv'.format(saved_folder_path, file[:-7].replace(" ","")),index=False) #-7 to remove '.en.vtt'
+        #remove files from local drive
+        os.remove(os.path.join(saved_folder_path,file))
+
+
