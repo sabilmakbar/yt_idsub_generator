804760d6ee463f7c4d08234db0abe05b2bfe98cc
sabilmakbar <maulana.1998@yahoo.co.id>
2022-12-23 23:03:44 +0700

    Tidying and fixing bug on main.py

--- Youtube_ASR_Scrapper/main.py
+++ Youtube_ASR_Scrapper/main.py
@@ -1,9 +1,7 @@
 # %% Package Import
 import os
-from pathlib import Path
+import pickle
 import pandas as pd
-import pickle as pkl
-from datetime import datetime
 
 try:
     from yt_scraper_class import YTScrapperDF, YTDataScrapper as YTGeneralScrapper
@@ -26,9 +24,10 @@ scrapper_general = YTGeneralScrapper()
 splitter_class_nn = TextSplitter(lang="en")
 
 #pickle versioning is important bcs of different versions could lead to pickling error
-print("The version of pickle used is {}.".format(pkl.format_version))
+print("The version of pickle used is {}.".format(pickle.format_version))
+print("The version of pandas used is {}.".format(pd.__version__))
 
-# %% 
+# %%
 
 load_checkpoint_file = link_scrapper_params["checkpoint_bool"]
 save_path_links = link_scrapper_params["save_load_path"]
@@ -46,9 +45,9 @@ channel_whitelist = meta_scrapper_params["whitelisted_channels"]
 video_title_keyword = meta_scrapper_params["whitelisted_titles_phrase"]
 
 load_checkpoint_file = meta_scrapper_params["checkpoint_bool"]
-save_path = meta_scrapper_params["save_load_path"]
+save_path_with_meta = meta_scrapper_params["save_load_path"]
 
-output = scrapper_df.df_loader_or_dumper(save_path_links, load_checkpoint_file, scrapper_df.video_result_filterer, df_input=output, 
+output = scrapper_df.df_loader_or_dumper(save_path_with_meta, load_checkpoint_file, scrapper_df.video_result_filterer, df_input=output,
                                          channel_col_name="channel_url", title_col_name="title", whitelisted_channels=channel_whitelist, whitelisted_title=video_title_keyword)
 
 # %%
@@ -59,14 +58,17 @@ download_folder_path = subtitle_scrapper_params["save_load_path"]
 
 yt_dlp_options = subtitle_scrapper_params["yt_dlp_options"]
 
+# %%
 if do_scrape:
     scrapper_general.yt_subtitle_downloader(download_lists, download_folder_path, yt_dlp_options)
-    if len(os.listdir(download_folder_path) != len(download_lists)):
+    if len(os.listdir(download_folder_path)) != len(download_lists):
         raise AssertionError("The length of the downloaded subtitle doesn't match with its expected!")
 
 # %%
-do_data_process = not(subtitle_scrapper_params["checkpoint_bool_processing"])
+do_data_process = subtitle_scrapper_params["checkpoint_bool_processing"]
 
 save_final_path = subtitle_scrapper_params["save_final_path"]
 
-output = scrapper_df.df_loader_or_dumper(save_final_path, do_data_process, scrapper_df.split_yt_subtitles, splitter=splitter_class_nn, df_path=download_folder_path)
+output = scrapper_df.df_loader_or_dumper(save_final_path, do_data_process, scrapper_df.scrapper_split_yt_subtitles, splitter=splitter_class_nn, df_path=download_folder_path, subtitle_col_name="text", start_stamp_col_name="start", stop_stamp_col_name="stop")
+
+# %%
