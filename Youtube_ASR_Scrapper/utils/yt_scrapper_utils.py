951bb303962f867ffc24bc01e3d20daee1bcde3f
Salsabil Maulana Akbar <maulana.1998@yahoo.co.id>
2022-12-24 15:05:23 +0700

    Update yt_scrapper_utils.py

--- Youtube_ASR_Scrapper/utils/yt_scrapper_utils.py
+++ Youtube_ASR_Scrapper/utils/yt_scrapper_utils.py
@@ -9,7 +9,6 @@ import subprocess
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 from requests_html import AsyncHTMLSession
